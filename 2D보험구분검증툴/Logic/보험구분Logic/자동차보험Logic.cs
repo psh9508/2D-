5da0ddae84c3fe92882901061640369e6082c355
박성현 <psh9508@ubcare.co.kr>
2018-07-16 14:29:49 +0900

    어디나 올리나?

--- 2D보험구분검증툴/Logic/보험구분Logic/자동차보험Logic.cs
+++ 2D보험구분검증툴/Logic/보험구분Logic/자동차보험Logic.cs
@@ -9,9 +9,9 @@ namespace _2D보험구분검증툴.Logic.보험구분Logic
 {
     class 자동차보험Logic : I보험구분검증
     {
-        public 오류목록Model GetErrorModel(int cnt)
+        public IEnumerable<오류목록Model> GetErrorModel(BarcodeModel model, int cnt)
         {
-            throw new NotImplementedException();
+            return new List<오류목록Model> { new 오류목록Model { No = ++cnt, 유형 = "보험구분", 메세지 = "자동차보험 바코드는 보험구분이 4, 공상및보훈구분에 값이 없어야합니다." } };
         }
 
         public bool Validation(BarcodeModel model)
